791debb5fc79a680680ae852cd2fc5bfff7c381c
Filip <Fpetersson96@gmail.com>
2018-03-15 10:34:02 +0100

    initialized cshop project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
